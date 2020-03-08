90779611757283f0b2e4ba1f600d03254a4421c0
attila <attila@matterpool.io>
2020-03-08 00:57:49 -0700

    Build

--- dist/boostpow.js
+++ dist/boostpow.js
@@ -1,6 +1,6 @@
 /**
  * boostpow-js - BoostPow Javascript SDK - matterpool.io
- * @version v1.0.18
+ * @version v1.0.19
  * @link https://github.com/MatterPool/boostpow-js#readme
  *
  * Copyright (c) 2020 MatterPool Inc.
