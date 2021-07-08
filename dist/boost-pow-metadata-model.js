767d1e29510d4d22375a66d58388d61adeedb87b
Restyled.io <commits@restyled.io>
2021-07-08 17:24:18 +0000

    Restyled by prettier

--- dist/boost-pow-metadata-model.js
+++ dist/boost-pow-metadata-model.js
@@ -1,11 +1,17 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", {value : true});
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.BoostPowMetadataModel = void 0;
 const bsv = require("bsv");
 const boost_utils_1 = require("./boost-utils");
 class BoostPowMetadataModel {
-  constructor(tag, minerPubKeyHash, extraNonce1, extraNonce2, userNonce,
-              additionalData) {
+  constructor(
+    tag,
+    minerPubKeyHash,
+    extraNonce1,
+    extraNonce2,
+    userNonce,
+    additionalData
+  ) {
     this.tag = tag;
     this.minerPubKeyHash = minerPubKeyHash;
     this.extraNonce1 = extraNonce1;
@@ -15,109 +21,152 @@ class BoostPowMetadataModel {
   }
   static fromObject(params) {
     return new BoostPowMetadataModel(
-        new Buffer(params.tag, 'hex'),
-        new Buffer(params.minerPubKeyHash, 'hex'),
-        boost_utils_1.BoostUtils.createBufferAndPad(params.extraNonce1, 4,
-                                                    false),
-        boost_utils_1.BoostUtils.createBufferAndPad(params.extraNonce2, 8,
-                                                    false),
-        boost_utils_1.BoostUtils.createBufferAndPad(params.userNonce, 4, false),
-        new Buffer(params.additionalData, 'hex'));
+      new Buffer(params.tag, "hex"),
+      new Buffer(params.minerPubKeyHash, "hex"),
+      boost_utils_1.BoostUtils.createBufferAndPad(params.extraNonce1, 4, false),
+      boost_utils_1.BoostUtils.createBufferAndPad(params.extraNonce2, 8, false),
+      boost_utils_1.BoostUtils.createBufferAndPad(params.userNonce, 4, false),
+      new Buffer(params.additionalData, "hex")
+    );
   }
   static fromBuffer(params) {
-    return new BoostPowMetadataModel(params.tag, params.minerPubKeyHash,
-                                     params.extraNonce1, params.extraNonce2,
-                                     params.userNonce, params.additionalData);
+    return new BoostPowMetadataModel(
+      params.tag,
+      params.minerPubKeyHash,
+      params.extraNonce1,
+      params.extraNonce2,
+      params.userNonce,
+      params.additionalData
+    );
   }
   trimBufferString(str, trimLeadingNulls = true) {
-    const content = Buffer.from(str, 'hex').toString('utf8');
+    const content = Buffer.from(str, "hex").toString("utf8");
     if (trimLeadingNulls) {
-      return content.replace(/\0/g, '');
+      return content.replace(/\0/g, "");
     } else {
       return content;
     }
   }
-  getTag() { return this.tag; }
+  getTag() {
+    return this.tag;
+  }
   getTagUtf8() {
     return this.trimBufferString(
-        Buffer.from(this.tag).reverse().toString('hex'), true);
+      Buffer.from(this.tag).reverse().toString("hex"),
+      true
+    );
   }
-  getTagString() { return this.getTagUtf8(); }
-  getMinerPubKeyHash() { return this.minerPubKeyHash; }
-  getMinerPubKeyHashUtf8() { return this.minerPubKeyHash.toString('hex'); }
-  getUserNonce() { return this.userNonce; }
+  getTagString() {
+    return this.getTagUtf8();
+  }
+  getMinerPubKeyHash() {
+    return this.minerPubKeyHash;
+  }
+  getMinerPubKeyHashUtf8() {
+    return this.minerPubKeyHash.toString("hex");
+  }
+  getUserNonce() {
+    return this.userNonce;
+  }
   getUserNonceUtf8() {
     return this.trimBufferString(
-        Buffer.from(this.userNonce).reverse().toString('hex'), true);
+      Buffer.from(this.userNonce).reverse().toString("hex"),
+      true
+    );
   }
-  getUserNonceNumber() { return this.userNonce.readUInt32LE(); }
+  getUserNonceNumber() {
+    return this.userNonce.readUInt32LE();
+  }
   getExtraNonce1Number() {
-    return parseInt(this.extraNonce1.toString('hex'), 16);
+    return parseInt(this.extraNonce1.toString("hex"), 16);
   }
-  getExtraNonce1() { return this.extraNonce1; }
+  getExtraNonce1() {
+    return this.extraNonce1;
+  }
   getExtraNonce2Number() {
-    return parseInt(this.extraNonce2.toString('hex'), 16);
+    return parseInt(this.extraNonce2.toString("hex"), 16);
   }
-  getExtraNonce2() { return this.extraNonce2; }
-  getAdditionalData() { return this.additionalData; }
+  getExtraNonce2() {
+    return this.extraNonce2;
+  }
+  getAdditionalData() {
+    return this.additionalData;
+  }
   getAdditionalDataUtf8() {
     return this.trimBufferString(
-        Buffer.from(this.additionalData).reverse().toString('hex'), true);
+      Buffer.from(this.additionalData).reverse().toString("hex"),
+      true
+    );
   }
-  getAdditionalDataString() { return this.getAdditionalDataUtf8(); }
+  getAdditionalDataString() {
+    return this.getAdditionalDataUtf8();
+  }
   toString() {
-    return Buffer
-        .concat([
-          this.tag, this.minerPubKeyHash, this.extraNonce1, this.extraNonce2,
-          this.userNonce, this.additionalData
-        ])
-        .toString('hex');
+    return Buffer.concat([
+      this.tag,
+      this.minerPubKeyHash,
+      this.extraNonce1,
+      this.extraNonce2,
+      this.userNonce,
+      this.additionalData,
+    ]).toString("hex");
   }
-  getCoinbaseString() { return this.toString(); }
+  getCoinbaseString() {
+    return this.toString();
+  }
   hash() {
     return bsv.crypto.Hash.sha256sha256(this.toBuffer())
-        .reverse()
-        .toString('hex');
+      .reverse()
+      .toString("hex");
   }
   hashAsBuffer() {
     return bsv.crypto.Hash.sha256sha256(this.toBuffer()).reverse();
   }
   toObject() {
     return {
-      tag : (this.tag.toString('hex').match(/../g) || []).reverse().join(''),
-      minerPubKeyHash :
-          (this.minerPubKeyHash.toString('hex').match(/../g) || [])
-              .reverse()
-              .join(''),
-      extraNonce1 : (this.extraNonce1.toString('hex').match(/../g) || [])
-                        .reverse()
-                        .join(''),
-      extraNonce2 : (this.extraNonce2.toString('hex').match(/../g) || [])
-                        .reverse()
-                        .join(''),
-      userNonce : (this.userNonce.toString('hex').match(/../g) || [])
-                      .reverse()
-                      .join(''),
-      additionalData : (this.additionalData.toString('hex').match(/../g) || [])
-                           .reverse()
-                           .join(''),
+      tag: (this.tag.toString("hex").match(/../g) || []).reverse().join(""),
+      minerPubKeyHash: (this.minerPubKeyHash.toString("hex").match(/../g) || [])
+        .reverse()
+        .join(""),
+      extraNonce1: (this.extraNonce1.toString("hex").match(/../g) || [])
+        .reverse()
+        .join(""),
+      extraNonce2: (this.extraNonce2.toString("hex").match(/../g) || [])
+        .reverse()
+        .join(""),
+      userNonce: (this.userNonce.toString("hex").match(/../g) || [])
+        .reverse()
+        .join(""),
+      additionalData: (this.additionalData.toString("hex").match(/../g) || [])
+        .reverse()
+        .join(""),
     };
   }
   toBuffer() {
     return Buffer.concat([
-      this.tag, this.minerPubKeyHash, this.extraNonce1, this.extraNonce2,
-      this.userNonce, this.additionalData
+      this.tag,
+      this.minerPubKeyHash,
+      this.extraNonce1,
+      this.extraNonce2,
+      this.userNonce,
+      this.additionalData,
     ]);
   }
-  toHex() { return this.toBuffer().toString('hex'); }
-  static fromString(str) { return BoostPowMetadataModel.fromHex(str); }
+  toHex() {
+    return this.toBuffer().toString("hex");
+  }
+  static fromString(str) {
+    return BoostPowMetadataModel.fromHex(str);
+  }
   static fromHex(str) {
-    return new BoostPowMetadataModel(Buffer.from(str.substr(0, 40), 'hex'),
-                                     Buffer.from(str.substr(40, 40), 'hex'),
-                                     Buffer.from(str.substr(80, 8), 'hex'),
-                                     Buffer.from(str.substr(88, 8), 'hex'),
-                                     Buffer.from(str.substr(96, 8), 'hex'),
-                                     Buffer.from(str.substr(104), 'hex'));
+    return new BoostPowMetadataModel(
+      Buffer.from(str.substr(0, 40), "hex"),
+      Buffer.from(str.substr(40, 40), "hex"),
+      Buffer.from(str.substr(80, 8), "hex"),
+      Buffer.from(str.substr(88, 8), "hex"),
+      Buffer.from(str.substr(96, 8), "hex"),
+      Buffer.from(str.substr(104), "hex")
+    );
   }
 }
 exports.BoostPowMetadataModel = BoostPowMetadataModel;
--- test/boost-pow-metadata.test.js
+++ test/boost-pow-metadata.test.js
@@ -1,76 +1,78 @@
-'use strict';
-var expect = require('chai').expect;
-var index = require('../dist/index.js');
-var bsv = require('bsv');
-describe('boost #BoostPowMetadata tests', () => {
-  it('should success create', async () => {
+"use strict";
+var expect = require("chai").expect;
+var index = require("../dist/index.js");
+var bsv = require("bsv");
+describe("boost #BoostPowMetadata tests", () => {
+  it("should success create", async () => {
     const abstract = index.BoostPowMetadata.fromObject({
-      tag : '01',
-      minerPubKeyHash : '00000000000000000000000000000000000000a4',
-      extraNonce1 : '014e',
-      extraNonce2 : '21a0',
-      userNonce : '00000011',
-      additionalData : '000000000000000000000000042',
+      tag: "01",
+      minerPubKeyHash: "00000000000000000000000000000000000000a4",
+      extraNonce1: "014e",
+      extraNonce2: "21a0",
+      userNonce: "00000011",
+      additionalData: "000000000000000000000000042",
     });
 
     const obj = abstract.toObject();
     expect(obj).to.eql({
-      tag : '01',
-      minerPubKeyHash : '00000000000000000000000000000000000000a4',
-      extraNonce1 : '014e0000',
-      extraNonce2 : '21a00000',
-      userNonce : '00000011',
-      additionalData : '000000000000000000000000042',
+      tag: "01",
+      minerPubKeyHash: "00000000000000000000000000000000000000a4",
+      extraNonce1: "014e0000",
+      extraNonce2: "21a00000",
+      userNonce: "00000011",
+      additionalData: "000000000000000000000000042",
     });
   });
 
-  it('should success create hex ', async () => {
+  it("should success create hex ", async () => {
     const abstract = index.BoostPowMetadata.fromObject({
-      tag : '0000000000000000000000000000000000000001',
-      minerPubKeyHash : '00000000000000000000000000000000000000a4',
-      extraNonce1 : '4e01',
-      extraNonce2 : 'a021',
-      userNonce : '01000000',
-      additionalData :
-          '0000000000000000000000000000000000000000000000000000000000000042',
+      tag: "0000000000000000000000000000000000000001",
+      minerPubKeyHash: "00000000000000000000000000000000000000a4",
+      extraNonce1: "4e01",
+      extraNonce2: "a021",
+      userNonce: "01000000",
+      additionalData:
+        "0000000000000000000000000000000000000000000000000000000000000042",
     });
 
     const obj = abstract.toObject();
     expect(obj).to.eql({
-      tag : '0000000000000000000000000000000000000001',
-      minerPubKeyHash : '00000000000000000000000000000000000000a4',
-      extraNonce1 : '4e010000',
-      extraNonce2 : 'a0210000',
-      userNonce : '01000000',
-      additionalData :
-          '0000000000000000000000000000000000000000000000000000000000000042',
+      tag: "0000000000000000000000000000000000000001",
+      minerPubKeyHash: "00000000000000000000000000000000000000a4",
+      extraNonce1: "4e010000",
+      extraNonce2: "a0210000",
+      userNonce: "01000000",
+      additionalData:
+        "0000000000000000000000000000000000000000000000000000000000000042",
     });
-    expect(abstract.toHex())
-        .to.eql(
-            '0100000000000000000000000000000000000000a4000000000000000000000000000000000000004e010000a0210000010000004200000000000000000000000000000000000000000000000000000000000000');
+    expect(abstract.toHex()).to.eql(
+      "0100000000000000000000000000000000000000a4000000000000000000000000000000000000004e010000a0210000010000004200000000000000000000000000000000000000000000000000000000000000"
+    );
     const fromHex = index.BoostPowMetadata.fromHex(
-        '0100000000000000000000000000000000000000a4000000000000000000000000000000000000004e010000a0210000010000004200000000000000000000000000000000000000000000000000000000000000');
+      "0100000000000000000000000000000000000000a4000000000000000000000000000000000000004e010000a0210000010000004200000000000000000000000000000000000000000000000000000000000000"
+    );
     expect(abstract.toHex()).to.eql(fromHex.toHex());
     expect(fromHex.toObject()).to.eql(obj);
   });
 
-  it('should success create and get hash of abstract', async () => {
+  it("should success create and get hash of abstract", async () => {
     const fromHex = index.BoostPowMetadata.fromHex(
-        '0100000000000000000000000000000000000000a4000000000000000000000000000000000000004e010000a0210000010000004200000000000000000000000000000000000000000000000000000000000000');
-    expect(fromHex.hash())
-        .to.eql(
-            '2dd2ce3a9bd404105a56433e1e0ce8cfa458e0a3669ce45f56132fc23d18a125');
+      "0100000000000000000000000000000000000000a4000000000000000000000000000000000000004e010000a0210000010000004200000000000000000000000000000000000000000000000000000000000000"
+    );
+    expect(fromHex.hash()).to.eql(
+      "2dd2ce3a9bd404105a56433e1e0ce8cfa458e0a3669ce45f56132fc23d18a125"
+    );
   });
 
-  it('should return same utf8 values each time', async () => {
+  it("should return same utf8 values each time", async () => {
     const abstract = index.BoostPowMetadata.fromObject({
-      tag : '010203',
-      minerPubKeyHash : '00000000000000000000000000000000000000a4',
-      extraNonce1 : '014e',
-      extraNonce2 : '21a0',
-      userNonce : '00091011',
-      additionalData :
-          '0000000000000000000000000000000000000000000000000000000000404142',
+      tag: "010203",
+      minerPubKeyHash: "00000000000000000000000000000000000000a4",
+      extraNonce1: "014e",
+      extraNonce2: "21a0",
+      userNonce: "00091011",
+      additionalData:
+        "0000000000000000000000000000000000000000000000000000000000404142",
     });
 
     const tagString1 = abstract.getTagUtf8();
@@ -88,88 +90,90 @@ describe('boost #BoostPowMetadata tests', () => {
 
     const obj = abstract.toObject();
     expect(obj).to.eql({
-      tag : '0000000000000000000000000000000000010203',
-      minerPubKeyHash : '00000000000000000000000000000000000000a4',
-      extraNonce1 : '0000014e',
-      extraNonce2 : '000021a0',
-      userNonce : '00091011',
-      additionalData :
-          '0000000000000000000000000000000000000000000000000000000000404142',
+      tag: "0000000000000000000000000000000000010203",
+      minerPubKeyHash: "00000000000000000000000000000000000000a4",
+      extraNonce1: "0000014e",
+      extraNonce2: "000021a0",
+      userNonce: "00091011",
+      additionalData:
+        "0000000000000000000000000000000000000000000000000000000000404142",
     });
   });
 });
 
-describe('boost #BoostPowJob createBoostPowMetadata', () => {
+describe("boost #BoostPowJob createBoostPowMetadata", () => {
   // https://search.matterpool.io/tx/600834a5c14436aa1b369cf9780994f988a7f0bb30e9e4e0bc6dedc1598e8ede
-  it('createBoostPowMetadata success 600834a5c14436aa1b369cf9780994f988a7f0bb30e9e4e0bc6dedc1598e8ede',
-     async () => {
-       const job = index.BoostPowJob.fromRawTransaction(
-           '01000000018ff2fe10e8629051853507b4189bf3981569a0d358e0506033a11618f2e3b10c010000006b483045022100f82288631d8c8b6b6fba9094a6d56af6ab572347b7365dcf7e6d68905cb8fd000220390cde292cc50a92bd60e680bfcbddf17443d904c7372880b6ec312a06952fb3412102be82a62c8c3d8e949c9b54a60b4cadf0efacec08164b3eca3b6e793f52bf8d8affffffff0220090000000000001976a914cdb2b66b5fa33fa3f55fb9296f31d445892d990988ace218000000000000e108626f6f7374706f7775047800000020325593000000000000000000000000000000000000000000000000000000000004ffff001d14231200000000000000000000000000000000000004886600002094000000000000000000000000000000000000000000000000000000000000007e7c557a766b7e52796b557a8254887e557a8258887e7c7eaa7c6b7e7e7c8254887e6c7e7c8254887eaa01007e816c825488537f7681530121a5696b768100a0691d00000000000000000000000000000000000000000000000000000000007e6c539458959901007e819f6976a96c88ac00000000',
-           1);
+  it("createBoostPowMetadata success 600834a5c14436aa1b369cf9780994f988a7f0bb30e9e4e0bc6dedc1598e8ede", async () => {
+    const job = index.BoostPowJob.fromRawTransaction(
+      "01000000018ff2fe10e8629051853507b4189bf3981569a0d358e0506033a11618f2e3b10c010000006b483045022100f82288631d8c8b6b6fba9094a6d56af6ab572347b7365dcf7e6d68905cb8fd000220390cde292cc50a92bd60e680bfcbddf17443d904c7372880b6ec312a06952fb3412102be82a62c8c3d8e949c9b54a60b4cadf0efacec08164b3eca3b6e793f52bf8d8affffffff0220090000000000001976a914cdb2b66b5fa33fa3f55fb9296f31d445892d990988ace218000000000000e108626f6f7374706f7775047800000020325593000000000000000000000000000000000000000000000000000000000004ffff001d14231200000000000000000000000000000000000004886600002094000000000000000000000000000000000000000000000000000000000000007e7c557a766b7e52796b557a8254887e557a8258887e7c7eaa7c6b7e7e7c8254887e6c7e7c8254887eaa01007e816c825488537f7681530121a5696b768100a0691d00000000000000000000000000000000000000000000000000000000007e6c539458959901007e819f6976a96c88ac00000000",
+      1
+    );
 
-       const userNonce = '00006688';
-       const tag = '0000000000000000000000000000000000001223';
-       const additionalData =
-           '0000000000000000000000000000000000000000000000000000000000000094';
-       const content =
-           '0000000000000000000000000000000000000000000000000000000000935532';
-       const extraNonce1Int = 1174405125;
-       const extraNonce1Hex = Buffer.from(extraNonce1Int.toString(16), 'hex')
-                                  .reverse()
-                                  .toString('hex');
-       const extraNonce2Hex = '0000000000000000';
-       expect(job.toObject()).to.eql({
-         content : content,
-         diff : 1,
-         category : '00000078',
-         tag : tag,
-         additionalData : additionalData,
-         userNonce : userNonce,
-       });
-       var expectedPubKeyHash = '92e4d5ab4bb067f872d28f44d3e5433e56fca190';
-       const nonceHex = 'e2731ee0';
-       const timeHex = '5e802ed9';
-       const jobProof = index.BoostPowJobProof.fromObject({
-         signature : '00',
-         minerPubKey :
-             '02f96821f6d9a6150e0ea06b00c8c77597e863330041be70438ff6fb211d7efe66',
-         extraNonce1 : Buffer.from(extraNonce1Hex, 'hex').toString('hex'),
-         extraNonce2 : Buffer.from(extraNonce2Hex, 'hex').toString('hex'),
-         time : Buffer.from(timeHex, 'hex').toString('hex'),
-         nonce : Buffer.from(nonceHex, 'hex').toString('hex'),
-         minerPubKeyHash :
-             Buffer.from(expectedPubKeyHash, 'hex').toString('hex'),
-       });
+    const userNonce = "00006688";
+    const tag = "0000000000000000000000000000000000001223";
+    const additionalData =
+      "0000000000000000000000000000000000000000000000000000000000000094";
+    const content =
+      "0000000000000000000000000000000000000000000000000000000000935532";
+    const extraNonce1Int = 1174405125;
+    const extraNonce1Hex = Buffer.from(extraNonce1Int.toString(16), "hex")
+      .reverse()
+      .toString("hex");
+    const extraNonce2Hex = "0000000000000000";
+    expect(job.toObject()).to.eql({
+      content: content,
+      diff: 1,
+      category: "00000078",
+      tag: tag,
+      additionalData: additionalData,
+      userNonce: userNonce,
+    });
+    var expectedPubKeyHash = "92e4d5ab4bb067f872d28f44d3e5433e56fca190";
+    const nonceHex = "e2731ee0";
+    const timeHex = "5e802ed9";
+    const jobProof = index.BoostPowJobProof.fromObject({
+      signature: "00",
+      minerPubKey:
+        "02f96821f6d9a6150e0ea06b00c8c77597e863330041be70438ff6fb211d7efe66",
+      extraNonce1: Buffer.from(extraNonce1Hex, "hex").toString("hex"),
+      extraNonce2: Buffer.from(extraNonce2Hex, "hex").toString("hex"),
+      time: Buffer.from(timeHex, "hex").toString("hex"),
+      nonce: Buffer.from(nonceHex, "hex").toString("hex"),
+      minerPubKeyHash: Buffer.from(expectedPubKeyHash, "hex").toString("hex"),
+    });
 
-       expect(jobProof.toObject()).to.eql({
-         "extraNonce1" : extraNonce1Hex,
-         "extraNonce2" : extraNonce2Hex,
-         "minerPubKey" :
-             '02f96821f6d9a6150e0ea06b00c8c77597e863330041be70438ff6fb211d7efe66',
-         "minerPubKeyHash" : expectedPubKeyHash,
-         "nonce" : nonceHex,
-         "signature" : "00",
-         "time" : timeHex,
-       });
-       expect(index.BoostPowJob.createBoostPowMetadata(job, jobProof)
-                  .toBuffer()
-                  .toString('hex'))
-           .to.eql(
-               '231200000000000000000000000000000000000092e4d5ab4bb067f872d28f44d3e5433e56fca190460000050000000000000000886600009400000000000000000000000000000000000000000000000000000000000000');
+    expect(jobProof.toObject()).to.eql({
+      extraNonce1: extraNonce1Hex,
+      extraNonce2: extraNonce2Hex,
+      minerPubKey:
+        "02f96821f6d9a6150e0ea06b00c8c77597e863330041be70438ff6fb211d7efe66",
+      minerPubKeyHash: expectedPubKeyHash,
+      nonce: nonceHex,
+      signature: "00",
+      time: timeHex,
+    });
+    expect(
+      index.BoostPowJob.createBoostPowMetadata(job, jobProof)
+        .toBuffer()
+        .toString("hex")
+    ).to.eql(
+      "231200000000000000000000000000000000000092e4d5ab4bb067f872d28f44d3e5433e56fca190460000050000000000000000886600009400000000000000000000000000000000000000000000000000000000000000"
+    );
 
-       const expectedMerkleRootMetaHash =
-           'fc6bee7b4b3be794c6f2a9a9e04786ec8ccedf118b31e3739e919e4e2841c484';
-       expect(bsv.crypto.Hash
-                  .sha256sha256(
-                      index.BoostPowJob.createBoostPowMetadata(job, jobProof)
-                          .toBuffer())
-                  .reverse()
-                  .toString('hex'))
-           .to.eql(expectedMerkleRootMetaHash);
+    const expectedMerkleRootMetaHash =
+      "fc6bee7b4b3be794c6f2a9a9e04786ec8ccedf118b31e3739e919e4e2841c484";
+    expect(
+      bsv.crypto.Hash.sha256sha256(
+        index.BoostPowJob.createBoostPowMetadata(job, jobProof).toBuffer()
+      )
+        .reverse()
+        .toString("hex")
+    ).to.eql(expectedMerkleRootMetaHash);
 
-       expect(index.BoostPowJob.createBoostPowMetadata(job, jobProof).hash())
-           .to.eql(expectedMerkleRootMetaHash);
-       /*
+    expect(
+      index.BoostPowJob.createBoostPowMetadata(job, jobProof).hash()
+    ).to.eql(expectedMerkleRootMetaHash);
+    /*
        {"method": "mining.submit", "params": ["abra.001", "3",
        "0000000000000000", "5e802ed9", "e2731ee0"], "id":4} Mar 29 05:15:52
        ip-172-31-47-53 sserverboost[15138]: I0329 05:15:52.473852 15138
@@ -227,20 +231,22 @@ describe('boost #BoostPowJob createBoostPowMetadata', () => {
        "600834a5c14436aa1b369cf9780994f988a7f0bb30e9e4e0bc6dedc1598e8ede",
        "vout": 1 }
   */
-       const powString =
-           index.BoostPowJob.tryValidateJobProof(job, jobProof, true);
+    const powString = index.BoostPowJob.tryValidateJobProof(
+      job,
+      jobProof,
+      true
+    );
 
-       expect(powString.boostPowString.hash())
-           .to.eql(
-               '00000000f3a3ce33b86e99236e561d8e641ad62f13277a77abef50a6673e9330');
-       const powMetadata =
-           index.BoostPowJob.createBoostPowMetadata(job, jobProof);
+    expect(powString.boostPowString.hash()).to.eql(
+      "00000000f3a3ce33b86e99236e561d8e641ad62f13277a77abef50a6673e9330"
+    );
+    const powMetadata = index.BoostPowJob.createBoostPowMetadata(job, jobProof);
 
-       expect(powString.boostPowString.metadataHash())
-           .to.eql(powMetadata.hash());
-       expect(powString.boostPowString.metadataHash())
-           .to.eql(index.BoostPowMetadata
-                       .fromString(powString.boostPowMetadata.toString())
-                       .hash());
-     });
+    expect(powString.boostPowString.metadataHash()).to.eql(powMetadata.hash());
+    expect(powString.boostPowString.metadataHash()).to.eql(
+      index.BoostPowMetadata.fromString(
+        powString.boostPowMetadata.toString()
+      ).hash()
+    );
+  });
 });
