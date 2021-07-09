9ec71b3bbe03f932d32b27c6d83700d77f49c4f9
Restyled.io <commits@restyled.io>
2021-07-09 14:03:27 +0000

    Restyled by clang-format

--- test/boost-signal.test.js
+++ test/boost-signal.test.js
@@ -1,7 +1,7 @@
 'use strict';
 var expect = require('chai').expect;
 var index = require('../dist/index.js');
-var bsv = require ('bsv');
+var bsv = require('bsv');
 describe('boost #BoostSignal tests', () => {
 
-});
+                                     });
