9968a5d919c6cd77fc4af08bef9020ad6c2d979b
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2022-03-23 05:41:53 -0500

    Update js index that includes newly exported functions.

--- dist/index.js
+++ dist/index.js
@@ -9,12 +9,24 @@ const boost_utils_1 = require("./boost-utils");
 const boost_graph_api_client_1 = require("./boost-graph-api-client");
 const boost_signal_model_1 = require("./boost-signal-model");
 const boost_signal_ranker_model_1 = require("./boost-signal-ranker-model");
+var bytes_1 = require("./fields/bytes");
+Object.defineProperty(exports, "Bytes", { enumerable: true, get: function () { return bytes_1.Bytes; } });
+var difficulty_1 = require("./fields/difficulty");
+Object.defineProperty(exports, "Difficulty", { enumerable: true, get: function () { return difficulty_1.Difficulty; } });
+var digest20_1 = require("./fields/digest20");
+Object.defineProperty(exports, "Digest20", { enumerable: true, get: function () { return digest20_1.Digest20; } });
+var digest32_1 = require("./fields/digest32");
+Object.defineProperty(exports, "Digest32", { enumerable: true, get: function () { return digest32_1.Digest32; } });
 var int32Little_1 = require("./fields/int32Little");
 Object.defineProperty(exports, "Int32Little", { enumerable: true, get: function () { return int32Little_1.Int32Little; } });
+var uint16Little_1 = require("./fields/uint16Little");
+Object.defineProperty(exports, "UInt16Little", { enumerable: true, get: function () { return uint16Little_1.UInt16Little; } });
 var uint32Little_1 = require("./fields/uint32Little");
 Object.defineProperty(exports, "UInt32Little", { enumerable: true, get: function () { return uint32Little_1.UInt32Little; } });
 var uint32Big_1 = require("./fields/uint32Big");
 Object.defineProperty(exports, "UInt32Big", { enumerable: true, get: function () { return uint32Big_1.UInt32Big; } });
+var uint64Big_1 = require("./fields/uint64Big");
+Object.defineProperty(exports, "UInt64Big", { enumerable: true, get: function () { return uint64Big_1.UInt64Big; } });
 const defaultOptions = {
     graph_api_url: 'https://graph.boostpow.com',
     // graph_api_url: 'http://localhost:3000',
